1be8b88423b9477098a57e00e9567551183e92f5
ErkinovMoxirbek <erkinov.0021@gmail.com>
2023-03-09 15:33:53 +0500

    initDataBase update

--- src/main/java/org/example/db/InitDataBase.java
+++ src/main/java/org/example/db/InitDataBase.java
@@ -13,7 +13,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class InitDataBase {
-
+private static ProfileRepository profileRepository;
+private static CardRepository cardRepository;
     public static void adminInit() {
 
         Profile profile = new Profile();
@@ -26,7 +27,6 @@ public class InitDataBase {
         profile.setRole(ProfileRole.ADMIN);
 
 
-        ProfileRepository profileRepository = ComponentContainer.profileRepository;
 
         Profile profile1 = profileRepository.getProfileByPhone(profile.getPhone());
         if (profile1 != null) {
@@ -45,12 +45,19 @@ public class InitDataBase {
         card.setCreatedDate(LocalDateTime.now());
         card.setStatus(GeneralStatus.ACTIVE);
 
-        CardRepository cardRepository = ComponentContainer.cardRepository;
         Card exists = cardRepository.getCardByNumber(card.getCardNumber());
 
         if (exists != null) {
             return;
         }
         cardRepository.save(card);
+    }
+
+    public void setProfileRepository(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    public void setCardRepository(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
     }
 }
